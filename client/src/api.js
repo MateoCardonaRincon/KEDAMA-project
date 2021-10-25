2f530636765e578c28fc536626de5c72662ad8ed
MateoCardonaRincon <mcardonari@unal.edu.co>
2021-10-25 10:04:37 -0500

    updates

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,11 @@
+console.log("env:", process.env.ROOT_URL);
 const callApi = async (url, reqOptions = {}) => {
   reqOptions.headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
     authorization: "Bearer " + localStorage.getItem("token"),
   };
-  const response = await fetch(
-    "https://kedama-project.herokuapp.com/api" + url,
-    reqOptions
-  );
+  const response = await fetch(process.env.ROOT_URL + url, reqOptions);
   const data = await response.json();
   return data;
 };
